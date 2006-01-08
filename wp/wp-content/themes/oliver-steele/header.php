e64b7de1dc9bd2124331fcd6accdb9c063d85c2d
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-01-08 16:32:45 +0000

    change the google ad format
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@56 7da26926-1605-0410-981f-d0d861a4a689

--- wp/wp-content/themes/oliver-steele/header.php
+++ wp/wp-content/themes/oliver-steele/header.php
@@ -17,7 +17,6 @@ urchinTracker();
 </script>
 
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-	<link rel="stylesheet" href="/projects/style.css" type="text/css" media="screen" />
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
--- wp/wp-content/themes/oliver-steele/single.php
+++ wp/wp-content/themes/oliver-steele/single.php
@@ -20,8 +20,9 @@
 <script type="text/javascript"><!--
 google_ad_client = "pub-7558884554835464";
 google_ad_width = 468;
-google_ad_height = 15;
-google_ad_format = "468x15_0ads_al_s";
+google_ad_height = 60;
+google_ad_format = "468x60_as";
+google_ad_type = "text_image";
 google_ad_channel ="3072123568";
 //--></script>
 <script type="text/javascript"
