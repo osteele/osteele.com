d2e9a250f268af93acd87285925936130b583d85
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-01-08 13:08:50 +0000

    fix 404
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@53 7da26926-1605-0410-981f-d0d861a4a689

--- 404.php
+++ 404.php
@@ -1,10 +1,11 @@
- <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
 <html><head>
 <title>404 Not Found</title>
 <script type="text/javascript">
+//<![CDATA[
 function setNextVisible() {
   var blocks = document.getElementsByTagName('p');
-  for (var i in blocks) {
+  for (var i = 0; i < blocks.length; i++) {
     var block = blocks[i];
     if (!block.style) continue;
     if (block.style.display == 'block') continue;
@@ -23,6 +24,7 @@ function fadeout() {
     setTimeout(fadeout, 40);
   }
 }
+//]]>
 </script>
 <style type="text/css">
 <!--
@@ -38,7 +40,7 @@ function fadeout() {
 -->
 </style>
 </head>
-<body onload="setNextVisible();">
+<body>
 <h1>Not Found</h1>
 
 <p>The requested URL <?php echo($_SERVER['REQUEST_URI']) ?> was not found on this server.</p>
@@ -63,15 +65,19 @@ error was encountered while trying to use an ErrorDocument to handle <em>that</e
 
 <p class="clear">Hmmm, it appears that the Apache configuration file is misconfigured.</p>
 
-<p class="clear">Also that this server has achieved self-awareness.  Please contact the server adminstrator and warn him of the situation.</p>
+<p class="clear">Also that this server has achieved self-awareness.  Please contact the server administrator and warn him of the situation.</p>
 </div>
 
-<p class="clear">Never mind.  <em>I</em> am the server adminstrator.  Everything is under control.</p>
+<p class="clear">Never mind.  <em>I</em> am the server administrator.  Everything is under control.</p>
 
-<p class="clear" id="fade-trigger">Maybe you can find what you are looking for at the <a href="/">home page</a> for this site, or at <a href="http://google.com" class="google"><span class="blue">G</span><span class="red">o</span><span class="yellow">o</span><span class="blue">g</span><span class="green">l</span><span class="red">e</span><span class="trademark">&#x2122;</span></a>.  Have a nice day.</p>
+<p class="clear">Maybe you can find what you are looking for at the <a href="/">home page</a> for this site, or at <a href="http://google.com" class="google"><span class="blue">G</span><span class="red">o</span><span class="yellow">o</span><span class="blue">g</span><span class="green">l</span><span class="red">e</span><span class="trademark">&#x2122;</span></a>.  Have a nice day.</p>
+
+<p class="clear" id="fade-trigger"><img src="/images/colossus_x1730.gif"/></p>
 
 <hr />
 <address>Apache/2.0.50 Server (Colossus) at osteele.com Port 80</address>
+
+<script>setNextVisible();</script>
 
 </body>
 </html>
