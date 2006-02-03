5666d6737aafb2fe66f92f4cfa2eadd3e9aaf3e9
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-02-03 19:08:53 +0000

    offline formatting for portfolio sources
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@100 7da26926-1605-0410-981f-d0d861a4a689

--- sources/portfolio/echo.php
+++ sources/portfolio/echo.php
@@ -7,10 +7,13 @@ if (preg_match('/\.java$/', $file))
 	$language = 'java';
 if (preg_match('/\.py$/', $file))
 	$language = 'python';
+//if (preg_match('/\.rb$/', $file))
+//	$language = 'ruby';
+//if (preg_match('/\.xml$/', $file))
+//	$language = 'xml';
 
 if ($language && $_GET['format']!='plain') {
-	exec('enscript --color -E'.$language.' --language=html --output=- '.$file.' --title '.$file, $output);
-	$content = join("\n", $output);
+	$content = file_get_contents('formatted/'.$file);
 	$content = preg_replace('|</h1>|i', ' <span style="font-size: large">(<a href="?format=plain" style="font-size: large">plain text</a><span style="font-size: large">)</span>\0', $content, 1);
 	echo $content;
  } else {
