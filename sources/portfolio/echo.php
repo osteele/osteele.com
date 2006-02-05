48a6d74f4be091c3c5f64efeaf8e02af6518e0de
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-02-05 01:05:38 +0000

    format lzx
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@105 7da26926-1605-0410-981f-d0d861a4a689

--- sources/portfolio/echo.php
+++ sources/portfolio/echo.php
@@ -1,25 +1,13 @@
 <?php
 $file = $_GET['source'];
+$formatted = 'formatted/'.$file;
 
-if (preg_match('/\.js$/', $file))
-	$language = 'javascript';
-if (preg_match('/\.java$/', $file))
-	$language = 'java';
-if (preg_match('/\.py$/', $file))
-	$language = 'python';
-if (preg_match('/\.rb$/', $file))
-	$language = 'ruby';
-if (preg_match('/\.php$/', $file))
-	$language = 'php';
-//if (preg_match('/\.xml$/', $file))
-//	$language = 'xml';
-
-if ($language && $_GET['format']!='plain') {
-	$content = file_get_contents('formatted/'.$file);
+if (file_exists($formatted) && $_GET['format']!='plain') {
+	$content = file_get_contents($formatted);
 	$content = preg_replace('|</h1>|i', ' <span style="font-size: large">(<a href="?format=plain" style="font-size: large">plain text</a><span style="font-size: large">)</span>\0', $content, 1);
 	echo $content;
  } else {
-	header("Content-type: text/text; charset=utf-8");
+	header("Content-type: text/plain; charset=utf-8");
 	echo(file_get_contents($file));
  }
 ?>
--- sources/portfolio/expialidocious/analyzer.js
+++ sources/portfolio/expialidocious/analyzer.js
@@ -1,5 +1,5 @@
 /*
-Copryight 2005-2006 Oliver steele.  Some rights reserved.
+Copyright 2005-2006 Oliver steele.  Some rights reserved.
 $LastChangedDate: 2006-01-07 15:24:44 -0500 (Sat, 07 Jan 2006) $
 
 This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 2.5 License:
@@ -24,34 +24,36 @@ var DataFrame = function () {
 DataFrame.prototype = {
   getColumnIndex: function (name) {
     var i = this.columnNameIndices[name];
-	// create new columns on demand
+    // create new columns on demand
     if (!(i >= 0)) {
       i = this.columnNameIndices[name] = this.columnNames.length;
-	  var column = new Array(this.rowNames.length);
-	  for (var j = 0; j < column.length; j++)
+      var column = new Array(this.rowNames.length);
+      for (var j = 0; j < column.length; j++)
         column[j] = 0;
       this.columnNames.push(name);
       this.columns.push(column);
     }
-	return i;
+    return i;
   },
   
   getRowIndex: function (name) {
     var j = this.rowNameIndices[name];
-	// create new rows on demand
-	if (!(j >= 0)) {
-	  j = this.rowNameIndices[name] = this.rowNames.length;
-	  this.rowNames.push(name);
-	  // fill up new rows, to keep the matrix rectangular
-	  for (var i = 0; i < this.columns.length; i++)
-		this.columns[i][j] = 0;
-	}
-	return j;
+    // create new rows on demand
+    if (!(j >= 0)) {
+      j = this.rowNameIndices[name] = this.rowNames.length;
+      this.rowNames.push(name);
+      // fill up new rows, to keep the matrix rectangular
+      for (var i = 0; i < this.columns.length; i++)
+        this.columns[i][j] = 0;
+    }
+    return j;
   },
   
+  // helper method for columnRangeSum.  This method iterates over all
+  // the rows.
   addColumns_iterate: function (a, b) {
-	var sum = new Array(this.columnNames.length);
-	for (var j = 0; j < sum.length; j++) sum[j] = 0;
+    var sum = new Array(this.columnNames.length);
+    for (var j = 0; j < sum.length; j++) sum[j] = 0;
     for (var i = a; i < b; i++) {
       var column = this.columns[i];
       for (var t = 0; t < column.length; t++)
@@ -60,41 +62,53 @@ DataFrame.prototype = {
     return sum;
   },
   
+  // helper method for columnRangeSum.  This method iterates or
+  // subdivides, depending on the range.  It recurses through _memoize
+  // to use the cache.  It only subdivides at binary subdivisions of
+  // the domain range ([left, right]), rather than the selection range
+  // ([a,b]).  This allows the cache to be reused when the selection
+  // changes.
   addColumns_choose: function (a, b, left, right) {
-    if (b - a < 4) return this.addColumns_iterate(a, b); // cutoff
-	var middle = Math.floor((left + right)/2);
-	var s0 = a < middle && this.addColumns_subdivide(a, Math.min(b, middle-1), left, middle);
-	var s1 = middle <= b && this.addColumns_subdivide(Math.max(a, middle), b, middle, right);
-	if (!s0 || !s1) return s0 || s1;
-	var sum = new Array(s0.length);
-	for (var i = 0; i < s0.length; i++)
-	  sum[i] = s0[i] + s1[i];
-	return sum;
+    // subdivision cutoff; empirically determined
+    if (b - a < 4) return this.addColumns_iterate(a, b);
+    var middle = Math.floor((left + right)/2);
+    var s0 = a < middle && this.addColumns_memoize(a, Math.min(b, middle-1), left, middle);
+    var s1 = middle <= b && this.addColumns_memoize(Math.max(a, middle), b, middle, right);
+    if (!s0 || !s1) return s0 || s1;
+    var sum = new Array(s0.length);
+    for (var i = 0; i < s0.length; i++)
+      sum[i] = s0[i] + s1[i];
+    return sum;
   },
   
-  addColumns_subdivide: function (a, b, left, right) {
-	if (arguments.length <= 2) {
-	  left = 0;
-	  right = this.columns.length;
-	}
-	var cache = this.columnSumCache;
-	var key = a == left && b == right-1 && [a,b];
-	if (key && cache[key]) return cache[key];
-	if (a==b) return null;
-	var sum = this.addColumns_choose(a, b, left, right);
-	if (key) cache[key] = sum;
-	return sum;
+  // helper method for columnRangeSum.  This method memoizes
+  // addcolumns_choose.  It only caches ranges in the binary
+  // subdivision tree of the domain.  This allows the cache
+  // to be reused when the range window changes.
+  addColumns_memoize: function (a, b, left, right) {
+    if (arguments.length <= 2) {
+      left = 0;
+      right = this.columns.length;
+    }
+    var cache = this.columnSumCache;
+	// only cache even subdivisions
+    var key = a == left && b == right-1 && [a,b];
+    if (key && cache[key]) return cache[key];
+    if (a==b) return null;
+    var sum = this.addColumns_choose(a, b, left, right);
+    if (key) cache[key] = sum;
+    return sum;
   },
-
+  
   columnRangeSum: function(a, b) {
-	//var t0 = (new Date).getTime();
-	sum = this.addColumns_subdivide(a, b);
-	//Debug.write((new Date).getTime()-t0);
-	return sum;
+    //var t0 = (new Date).getTime();
+    sum = this.addColumns_memoize(a, b);
+    //Debug.write((new Date).getTime()-t0);
+    return sum;
   },
   
   getColumnSums: function () {
-	var sums = new Array(this.columns.length);
+    var sums = new Array(this.columns.length);
     for (var i = 0; i < this.columns.length; i++) {
       var column = this.columns[i];
       var sum = 0;
@@ -114,9 +128,9 @@ function computeArrayinversion(source, target) {
   inversion = [];
   for (var i = 0; i < source.length; i++) {
     var tagname = source[i];
-	var j = 0;
-	while (target[j] != tagname) j++;
-	inversion.push(j);
+    var j = 0;
+    while (target[j] != tagname) j++;
+    inversion.push(j);
   }
   return inversion;
 }
@@ -131,7 +145,7 @@ function fillTagFrame(dataframe, posts) {
     var post = posts[i];
     var date = post.attributes['time'].split('T')[0];
     var tags = post.attributes['tag'].split(' ');
-	var di = dataframe.getColumnIndex(date);
+    var di = dataframe.getColumnIndex(date);
     var counts = dataframe.columns[di];
     for (var j in tags) {
       var tag = tags[j];
