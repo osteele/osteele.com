3acda013c8dd60b40b5ad07e76fbaa05f179f356
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-02-16 21:27:50 +0000

    deploy rematch
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@118 7da26926-1605-0410-981f-d0d861a4a689

---
+++ tools/rematch/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html
+  PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>reMatch</title>
+<style type="text/css">
+html, body { margin: 0; padding: 0; height: 100%; }
+#flashcontent { height: 100%; }
+.fallback { margin: 5px }
+</style></head>
+<script type="text/javascript" src="/javascript/flashobject.js"></script>
+<body>
+
+<div id="flashcontent">
+<div class="fallback">
+  <p>This application requires the Flash plugin.  If the plugin is already installed, click <a href="?detectflash=false">here</a>.</p>
+</div>
+</div>
+
+<script type="text/javascript">
+var fo = new FlashObject("graph.swf", "rematch", "100%", "100%", "7", "#FFFFFF");
+fo.addParam("scale", "noscale");
+fo.addVariable("source", "server");
+fo.addVariable("lzproxied", "false");
+fo.write("flashcontent");
+</script>
+
+<?php include('../../includes/footer.php'); ?>
