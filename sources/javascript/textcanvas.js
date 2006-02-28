cd0a42d1c3787bf7adb8810d0c2ed419f3095c40
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-02-28 01:33:18 +0000

    finish textdrawview; add docs to textcanvas
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@202 7da26926-1605-0410-981f-d0d861a4a689

--- sources/javascript/textcanvas.js
+++ sources/javascript/textcanvas.js
@@ -3,6 +3,71 @@
   Copyright: Copyright 2006 Oliver Steele.  All rights reserved.
   Homepage: http://osteele.com/sources/javascript
   License: MIT License.
+  
+  Description:
+    TextCanvasController provides an API similar to that of the
+    canvas element, but with the addition of a method
+      controller.getContext('2d').drawString(x, y, string);
+    that can be used to position strings on the canvas.
+    
+    The strings are implemented as HTML spans, which are
+    positioned absolutely in front of the canvas.  They
+    therefore don't behave exactly as though they were on
+    the canvas:
+    - they don't respect the current transform
+    - they don't respect 
+    - nontext elements that are drawn subsequent to
+      a string will be positioned over it, not under it
+    (This last point could be fixed by changing the implementation
+    to create a context that proxies the basis canvas, but creates
+    a new canvas overlay at the start of each drawing command
+    that follows a call to drawString and switches to proxying that
+    instead.)
+  
+  API:
+    === TextCanvasController
+    textCanvasController = new TextCanvasController(container)
+      Container is an HTML div.  The canvas and string spans
+      will be placed inside this div.
+    textCanvasController.setDimension(width, height)
+      Set the width and height of the canvas.
+    context = textCanvasController.getContext('2d')
+      Returns a 2D context, modified to accept the following methods:
+    
+    === textCanvasController.getContext('2d')
+    context.drawString(x, y, string)
+      Draw string at (x, y), with the font and text style properties
+      specified in context.style (below).
+    context.erase()
+      Erase the content of the canvas.  This is equivalent to
+      context.clearRect(0, 0, canvas.width, canvas.height),
+      except that it also removes any strings created by
+      context.drawString().
+    context.style
+      An instance of ElementCSSInlineStyle.  Calls to drawString
+      use the font and text properties in this style object.  (This
+      API is analogous to the stateful mechanism that the 2d context
+      provides for setting stroke and fill properties.)
+      
+      This implementation uses the container's style object
+      for this.  This won't have any effect if you only set the
+      font and style properties, but will have surprising results
+      if you set other properties.
+      
+  Usage:
+    // <div id="canvasContainer"></div>
+    var container = document.getElementById('canvasContainer');
+    var textCanvasController = new TextCanvasController(container);
+    var ctx = textCanvasController.getContext('2d');
+    ctx.moveTo(0, 0);
+    ctx.lineTo(100, 100);
+    ctx.stringStyle.color = 'red';
+    ctx.drawString(0, 0, "red");
+    ctx.stringStyle.color = 'blue';
+    ctx.drawString(100, 100, "blue");
+  
+  There is a standalone example at
+  http://osteele.com/sources/javascript/textcanvas-demo.html.
 */
 
 function TextCanvasController(container) {
@@ -51,13 +116,23 @@ TextCanvasController.prototype.attachMethods = function(ctx, controller) {
 	
 	ctx.clear = function () {
 		controller.clear();
-	}
+	};
+    
+    ctx.stringStyle = controller.container.style;
 };
+
+TextCanvasController.CSSStringProperties = 'color direction fontFamily fontSize fontSizeAdjust font-stretch font-style fontVariant fontWeight fontStyle font-variant font-weight letter-spacing  line-height text-align text-decoration text-indent text-shadow text-transform unicode-bidi white-space word-spacing'.split(' ');
 
 TextCanvasController.prototype.addLabel = function(x, y, string) {
 	var label = document.createElement('div');
 	var text = document.createTextNode(string);
 	label.appendChild(text);
+    var style = this.container.style;
+    var cssNames = TextCanvasController.CSSStringProperties;
+    for (var i = 0; i < cssNames.length; i++) {
+        var name = cssNames[i];
+        label.style[name] = style[name];
+    }
 	label.style.position = 'absolute';
 	label.style.left = x;
 	label.style.top = y;
