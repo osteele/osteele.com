4021b02fbb95e98f7fde614da24a689aa20595b0
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-03-01 23:12:00 +0000

    fix inline console for ie
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@218 7da26926-1605-0410-981f-d0d861a4a689

--- sources/javascript/inline-console.js
+++ sources/javascript/inline-console.js
@@ -27,8 +27,22 @@ function addConsole() {
         e = document.createElement('div');
         fv.appendChild(e);
     }
-    e.innerHTML = '<form id="debugger" action="#" method="get" onsubmit="evalField(\'eval\'); return false"><div><input type="button" onclick="evalField(\'eval\'); return false;" value="Eval"/><input type="text" size="80" id="eval" value="document" onkeyup="event.preventDefault(); return false;"/></div></form>';
+    e.innerHTML = CONSOLE_HTML;
 }
 
-window.addEventListener('load', addConsole, false);
-//addConsole();
+var CONSOLE_HTML = '<form id="debugger" action="#" method="get" onsubmit="evalField(\'eval\'); return false"><div><input type="button" onclick="evalField(\'eval\'); return false;" value="Eval"/><input type="text" size="80" id="eval" value="document" onkeyup="/*event.preventDefault(); */return false;"/></div></form>';
+
+if (window.addEventListener) {
+    window.addEventListener('load', addConsole, false);
+} else if (window.attachEvent) {
+    window.attachEvent('onload', addConsole);
+} else {
+    window.onload = (function() {
+        var nextfn = window.onload || function(){};
+        return function() {
+            addConsole();
+            nextfn();
+        }
+    })();
+}
+
