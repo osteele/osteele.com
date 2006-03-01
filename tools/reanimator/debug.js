f5d9abb7d3b7d129fa5708461d5c8369518b88f1
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-03-01 03:50:23 +0000

    readable js
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@211 7da26926-1605-0410-981f-d0d861a4a689

--- tools/reanimator/debug.js
+++ tools/reanimator/debug.js
@@ -5,54 +5,87 @@
   License: MIT License.
 */
 
-Array.prototype.toString = function() {
-    var segments = [];
-    for (var i = 0; i < this.length; i++)
-        segments.push(Object.toString(this[i]));
-    return '[' + segments.join(', ') + ']';
-};
-
-Object.toString = function (o) {
+Object.toString = function (value, options) {
+    if (arguments.length < 2)
+        options = Object.toString.options;
     var specials = {};
     specials[null] = 'null';
     specials[undefined] = 'undefined';
-    if (specials[o])
-        return specials[o];
-    if (typeof o == 'function') return 'function';
-    return o.toString();
+    if (specials[value])
+        return specials[value];
+    if (typeof value == 'string') {
+        var subs = [/\r/g, '\\r', /\n/g, '\\n', /\t/g, '\\t',
+                    /\f/g, '\\f'];
+        for (var i = 0; i < subs.length; ) {
+            var s = subs[i++];
+            value = value.replace(s, subs[i++]);
+        }
+        if (value.match(/\'/) && !value.match(/\"/))
+            return '"' + value + '"';
+        else
+            return "'" + value.replace(/\'/g, '\\\'') + "'";
+    }
+    if (typeof value == 'function') return 'function';
+    return value.toString();
 };
+Object.toString.options = {};
 
-Object.prototype.toString = function() {
+Object.prototype.toString = function(options) {
     var segments = [];
+    var count = 0;
     for (var p in this) {
-        var o = this[p];
-        if (o == this.__proto__[p]) continue;
+        var value = this[p];
+        if (value == this.__proto__[p]) continue;
         if (segments.length) segments.push(', ');
+        if (options && options.limit && ++count > options.limit) {
+            segments.join('...');
+            break;
+        }
         segments.push(p.toString());
         segments.push(': ');
-        segments.push(Object.toString(o));
+        segments.push(Object.toString(value, options));
     }
     return '{' + segments.join('') + '}';
 };
 
-function p() {
+Array.prototype.toString = function(options) {
     var segments = [];
-    for (var i = 0; i < arguments.length; i++)
-        segments.push(Object.toString(arguments[i]));
-    print(segments.join(', '));
+    for (var i = 0; i < this.length; i++) {
+        if (options && options.limit && i > options.limit) {
+            segments.join('...and ' + (this.length-i)+ 'more');
+            break;
+        }
+        segments.push(Object.toString(this[i], options));
+    }
+    return '[' + segments.join(', ') + ']';
+};
+
+var _basis_message =
+    (function() {
+        try {
+            var saved = {debug: debug, info: info, warn: warn, error: error};
+            return function(kind, message) {saved[kind](message)};
+        } catch (e) {
+            var fn;
+            try {fn=alert}
+            catch (e) {fn=print}
+            return function(kind, message) {fn(kind + ': ' + message)}
+        }
+    })();
+
+function _debug_message(kind, args) {
+    var segments = [];
+    for (var i = 0; i < args.length; i++) {
+        var value = args[i];
+        if (typeof value == 'string') {segments.push(value); continue}
+        segments.push(Object.toString(args[i]));
+    }
+    var msg = segments.join(', ');
+    _basis_message(kind, msg);
     return undefined;
 }
 
-/*
-b = new Bezier([{x: 56, y: 118}, {x: 68, y: 125}, {x: 83, y: 135}, {x: 96, y: 143}]);
-b = new Bezier([{x: 0, y: 0}, {x: 0, y: 100}, {x: 100, y: 100}, {x: 100, y: 0}]);
-//p(b.midpoint());
-
-p(b.atT(.5));
-
-path = new Path();
-path.addCubic(b.points);
-p(path.atT(0));
-p(path.atT(.5));
-p(path.atT(1));
-*/
+info = function() {_debug_message('info', arguments)}
+warn = function() {_debug_message('warn', arguments)}
+debug = function() {_debug_message('debug', arguments)}
+error = function() {_debug_message('error', arguments)}
