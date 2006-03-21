e137953fc678529a0328d3c37db7b350834da2fa
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-03-21 22:17:37 +0000

    fix blog for wp2
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@293 7da26926-1605-0410-981f-d0d861a4a689

--- wp/index.php
+++ wp/index.php
@@ -1,4 +1,6 @@
 <?php 
+$_SERVER['REQUEST_URI'] = preg_replace('|^/blog(.*)|', '\1', $_SERVER['REQUEST_URI']);
+
 /* Short and sweet */
 define('WP_USE_THEMES', true);
 require('./wp-blog-header.php');
