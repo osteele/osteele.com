55f683a37d6b84d68c3264fb89b7a35a48623b7d
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-03-22 01:00:33 +0000

    wp tagging
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@297 7da26926-1605-0410-981f-d0d861a4a689

--- wp/index.php
+++ wp/index.php
@@ -1,5 +1,7 @@
 <?php 
 $_SERVER['REQUEST_URI'] = preg_replace('|^/blog(.*)|', '\1', $_SERVER['REQUEST_URI']);
+$_SERVER['REQUEST_URI'] = preg_replace('|^/fb-(feed.*)|', '\1', $_SERVER['REQUEST_URI']);
+//die($_SERVER['REQUEST_URI']);
 
 /* Short and sweet */
 define('WP_USE_THEMES', true);
