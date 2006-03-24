4be2f00052be3f8b5798abd03eb4f34ef4dcc832
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-03-24 00:19:39 +0000

    memoization
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@308 7da26926-1605-0410-981f-d0d861a4a689

--- sources/javascript/memoize.js
+++ sources/javascript/memoize.js
@@ -3,31 +3,60 @@
   Copyright: Copyright 2006 Oliver Steele.  All rights reserved.
   Homepage: http://osteele.com/sources/javascript
   Download: http://osteele.com/sources/javascript/memoize.js
-  Docs: http://osteele.com/sources/javascript/docs/gradients
-  Example: http://osteele.com/sources/javascript/demos/gradients.html
+  Docs: http://osteele.com/sources/javascript/docs/memoize
+  Example: http://osteele.com/sources/javascript/demos/memoization.html
   License: MIT License.
   Created: 2006-03-23
   
+  == Features
+  * Automatic per-instance caching.  +a.m()+ and +a.m()+ will
+  use separate memo tables, to cover the case where the function
+  result depends on the .
+  * Caches results that are false-like (e.g. +0+, +null+, and <tt>''</tt>).
+  * Fast path for nonary method, e.g. +bezier.getLength()+.
+  * Correctly distinguishes among arguments that stringify the
+  same; e.g. +f(null)+, +f('')+, and +f([])+; +f([])+ and +f([''])+;
+  etc.
+  * Allows overriding the cache key generator with a custom key
+  generator
   
+  == Usage
+  === Memoizing a global function
+  === Memozing a method
+  === Resetting the memoization cache
+  === Using a custom key generator
 */
-  
+
+/*
+  Agenda:
+  - benchmark nonnary
+  - fix reset for instances
+  - remove +object+ function from memoize
+  - special case for nonnary with this
+  - test 'memoize' function
+*/
+
 Function.prototype.memoize = function(object, keyfn) {
-    if (arguments.length < 3) keyfn = arguments.callee.simpleSerializer;
-    var self = this, nonaryfn, value, values;
+    keyfn = keyfn || arguments.callee.simpleSerializer;
+    var self = this, nonaryfn, value, globalValues;
     var mfn = function() {
         if (!arguments.length) return nonaryfn();
         var key = new Array(arguments.length);
         for (var i = 0; i < arguments.length; i++)
             key[i] = keyfn(arguments[i]);
         key = key.join(',');
-        return key in values ? values[key] : values[key] = self.apply(object, arguments);
+		var values = globalValues;
+		if (this) values = this.$memoization || (this.$memoization = {});
+		// testing both 'key in values' and 'values[key]' doesn't
+		// cost extra in Firefox 1.5 and Safari 2.0.2.
+        return key in values ? values[key] : values[key] = self.apply(this, arguments);
     }
     mfn.reset = function() {
         nonaryfn = function() {
             value = self.apply(object);
             return (nonaryfn = function(){return value})();
         };
-        values = {};
+        globalValues = {};
     };
     mfn.reset();
     return mfn;
@@ -51,6 +80,6 @@ Function.prototype.memoize.simpleSerializer = function(value) {
     return String(value);
 };
 
-Object.prototype.memoize = function(name, keyfn) {
+Object.prototype.memoize = function(fn, keyfn) {
     this[name] = this[name].memoize(this, keyfn);
 };
