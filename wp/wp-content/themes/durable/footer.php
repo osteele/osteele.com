d0f837c7945f46660889cbac562f98dda7ed9a60
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-04-15 08:40:22 +0000

    wp urchin footer
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@344 7da26926-1605-0410-981f-d0d861a4a689

--- wp/wp-content/themes/durable/footer.php
+++ wp/wp-content/themes/durable/footer.php
@@ -65,5 +65,14 @@ if ($durable->option['colorchange'] != "false") {
 </div>
 <? } ?>
 
+<?php if ($_SERVER['HTTP_HOST'] != 'osteele.dev') { ?>
+    <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+    </script>
+    <script type="text/javascript">
+      _uacct = "UA-202010-1";
+      urchinTracker();
+    </script>
+<?php } ?>
+
 </body>
 </html>
