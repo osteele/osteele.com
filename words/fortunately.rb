1e25410a355fa2f7f4626eeb1659361e6a9429a6
osteele <osteele@7da26926-1605-0410-981f-d0d861a4a689>
2006-05-14 20:46:39 +0000

    fortunately: switch to google
    git-svn-id: svn+ssh://osteele.com/home/osteele/svn/osteele.com/trunk@349 7da26926-1605-0410-981f-d0d861a4a689

--- words/fortunately.rb
+++ words/fortunately.rb
@@ -1,13 +1,32 @@
 require 'open-uri'
 
+#GOOGLE_KEY = 'SDeulnhQFHIK7kikQwIQSapNA5Q0C9d'
+
 def search query, results=10
   query = query.gsub(/\W/) {|c| format '%%%02x', c[0]}
-  s = open("http://api.search.yahoo.com/WebSearchService/V1/webSearch?appid=fortunately&query=#{query}&type=phrase&results=#{results}&language=en") {|f|f.read}
-  s.scan(/<Result>(.*?)<\/Result>/).map do |result|
+  url = "http://api.search.yahoo.com/WebSearchService/V1/webSearch?appid=fortunately&query=#{query}&type=phrase&results=#{results}&language=en"
+  p url
+  s = open(url) {|f|f.read}
+  s.scan(/<Result>(.*?)<\/Result>/m).map do |result|
     Hash[*
-        result.first.scan(/<([^>]+)>(.*?)<\/\1>/).
+        result.first.scan(/<([^>]+)>(.*?)<\/\1>/m).
         map {|k,v| [k.downcase.intern, v]}.flatten]
   end
+end
+
+def search query, count=10, start=1
+  if count > 10
+    return search(query, 10, start) + search(query, count-10, start+10)
+  end
+  require 'cgi'
+  url = "http://www.google.com/search?hl=en&q=#{CGI::escape(query)}"
+  url += "&start=#{start}" if start>1
+  s = open(url) {|f| f.read}
+  #return s
+  s.scan(%r{<a class=l href="(.*?)".*?>(.*?)<.*?<td class=j>(.*?)\.\.\.}m).
+    map{|url,title,s| {:url => url,
+      :title => title,
+      :sentence => s.gsub(%r{<.*?>}, '').gsub("\r\n", '')}}[0...count]
 end
 
 #p search('Fortunately, Oliver')
@@ -27,17 +46,22 @@ end
 module Fortunately
   def self.filter_results term
     first_word = term[/\W*(\w*)/, 1]
-    results = search(term, 50).
-      select {|r| r[:summary]}.
-      each {|r| r[:summary].gsub!(/^\.*\s*/, '');
-                r[:summary].gsub!(/\s*\.*$/, '')}.
-      select {|r| i = r[:summary].index first_word;
-                r[:sentence] = r[:summary][i..-1] if i}.
-      each {|r| r[:sentence] = fix_sentence r[:sentence]}
-    index = Hash[*results.reverse.map{|r|[r[:sentence], r]}.flatten]
-    results = results.select {|r| index[r[:sentence]] == r}
-    results = results.select {|r| r[:sentence].split.length > 4}
-    results = results[0..[results.length,5].min]
+    results = search(term, 15)
+    if true
+      result = results.
+        reject {|r| r[:url] =~ /osteele\.com/}.
+        select {|r| r[:summary]}.
+        each {|r| r[:summary].gsub!(/^\.*\s*/, '');
+          r[:summary].gsub!(/\s*\.*$/, '')}.
+        select {|r| i = r[:summary].index first_word;
+          r[:sentence] = r[:summary][i..-1] if i}.
+        each {|r| r[:sentence] = fix_sentence r[:sentence]}
+      index = Hash[*results.reverse.map{|r|[r[:sentence], r]}.flatten]
+      results = results.select {|r| index[r[:sentence]] == r}
+      results = results.select {|r| r[:sentence].split.length > 4}
+      results = results[0..[results.length,5].min]
+    end
+    results
   end
   
   def self.interleave term
@@ -47,7 +71,7 @@ module Fortunately
     long.reverse!
     short.reverse!
     r = []
-    until short.empty?
+    while short.any?
       r << long.pop
       r << short.pop
     end
@@ -64,4 +88,4 @@ module Fortunately
   end
 end
 
-#puts story_for('Oliver')
+#puts Fortunately.story_for('Oliver')
