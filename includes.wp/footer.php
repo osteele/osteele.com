98c0c7103dc481b008c59fce0a1bd727ca7ded0b
Oliver Steele <steele@osteele.com>
2008-02-20 13:24:54 -0500

    add quantcom tracking

--- includes.wp/footer.php
+++ includes.wp/footer.php
@@ -1,10 +1,8 @@
 <?php if ($_SERVER['HTTP_HOST'] != 'osteele.dev') { ?>
-    <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-    </script>
-    <script type="text/javascript">
-      _uacct = "UA-202010-1";
-      urchinTracker();
-    </script>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+<script type="text/javascript">_uacct = "UA-202010-1";urchinTracker();</script>
+<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
+<script type="text/javascript">_qacct="p-52aYZlc3ACyMc";quantserve();</script>
 <?php } ?>
 <?php if (isset($_GET['debug']) && false) { ?>
     <a href="#" onclick="eraseLog(); return false">clear</a>
--- projects/footer.php
+++ projects/footer.php
@@ -1,22 +1,7 @@
-<?php if ($_SERVER['HTTP_HOST'] != 'osteele.dev') { ?>
-<script type="text/javascript"><!--
-google_ad_client = "pub-7558884554835464";
-google_ad_width = 728;
-google_ad_height = 90;
-google_ad_format = "728x90_as";
-google_ad_channel ="3072123568";
-//--></script>
-<script type="text/javascript"
-  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-
-</script>
-
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-202010-1";
-urchinTracker();
-</script>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+<script type="text/javascript">_uacct = "UA-202010-1";urchinTracker();</script>
+<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
+<script type="text/javascript">_qacct="p-52aYZlc3ACyMc";quantserve();</script>
 
 <script type='text/javascript' src='http://track.mybloglog.com/js/jsserv.php?mblID=2005120301543073'></script>
 	 <?php } ?>
