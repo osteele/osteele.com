ed90a7a32e68011a03390016a5f9ae1d017bf5b7
Oliver Steele <steele@osteele.com>
2009-02-04 10:14:53 -0600

    fix php error

--- projects/footer.php
+++ projects/footer.php
@@ -1,3 +1,4 @@
+<?php if ($_SERVER['HTTP_HOST'] != 'osteele.dev') { ?>
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
 <script type="text/javascript">_uacct = "UA-202010-1";urchinTracker();</script>
 <script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
