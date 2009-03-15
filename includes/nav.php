4c1c352eabea972f11c9b9321e069c95f8bd4f9a
Oliver Steele <steele@osteele.com>
2009-03-15 10:03:59 -0400

    +zazzle store

--- includes/nav.php
+++ includes/nav.php
@@ -2,17 +2,20 @@
   <li class="home">
     <a href='/'>Home</a>
   </li>
+  <li>
+    <a href='/archives/'>Archives</a>
+  </li>
   <li class="services">
     <a href='/services/'>Services</a>
   </li>
-  <li>
-    <a href='/archives/'>Archives</a>
+  <li class="projects">
+    <a href='/projects'>Projects</a>
   </li>
   <li class="sources">
     <a href='/sources/'>Sources</a>
   </li>
-  <li class="projects">
-    <a href='/projects'>Projects</a>
+  <li class="store">
+    <a href='http://zazzle.com/osteele*'>Store</a>
   </li>
   <li>
     <a href='/blog/'>Blog</a>
