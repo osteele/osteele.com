68cb26c5fb3a3aac4e9e07ba1f124659a4cc4fe2
Oliver Steele <steele@osteele.com>
2017-01-02 10:59:36 -0500

    revive source formatting

--- previous/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<?php if ($_SERVER['HTTP_HOST'] != 'osteele.dev') { ?>
-<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
-<script type="text/javascript">_uacct = "UA-202010-1";urchinTracker();</script>
-<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
-<script type="text/javascript">_qacct="p-52aYZlc3ACyMc";quantserve();</script>
-<?php } ?>
-  </body>
-</html>
