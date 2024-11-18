2b805f900c9cb9995f594085d8e376ada51266e1
Oliver Steele <steele@osteele.com>
2024-11-17 22:31:44 -0500

    Fix static build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: process.env.NODE_ENV === "production" ? "export" : undefined,
 };
 
 export default nextConfig;
